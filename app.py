afee3140e9eb96e34b7d5eaea3a644831a1f6bd5
tcostello74 <tcostello74@yahoo.com>
2023-04-23 13:12:05 -0400

    Steps 1-2

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Tyler"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route("/")
+def home_page():
+
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template("index.html", board=board)
